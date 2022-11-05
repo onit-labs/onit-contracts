e352cfc078751a48a5c62d82036c3d5521e00ea5
jamesmcc-design <JAMES.MCC94@HOTMAIL.CO.UK>
2022-11-05 17:17:56 +0100

    fix: factory interface update

--- src/interfaces/IForumGroupFactory.sol
+++ src/interfaces/IForumGroupFactory.sol
@@ -3,11 +3,11 @@
 pragma solidity ^0.8.13;
 
 /// @notice Forum Factory Interface.
-interface IForumFactory {
-	function deployGroup(
-		string memory name_,
-		string memory symbol_,
-		address[] calldata voters_,
-		uint8[4] memory govSettings_
-	) external payable returns (address);
+interface IForumGroupFactory {
+    function deployGroup(
+        string memory name_,
+        string memory symbol_,
+        address[] calldata voters_,
+        uint8[4] memory govSettings_
+    ) external payable returns (address);
 }
