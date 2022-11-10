268e91df6296dda0f7dee72d330dfc8577665bb6
jamesmcc-design <JAMES.MCC94@HOTMAIL.CO.UK>
2022-11-10 15:14:51 +0100

    wip: update deploy files

--- deploy/run/14_deploy_crowdfund_execution_manager.ts
+++ deploy/run/14_deploy_crowdfund_execution_manager.ts
@@ -7,12 +7,10 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 	const { deployer } = await hre.getNamedAccounts()
 	const { deterministic } = hre.deployments
 
-	const ForumFactory = await hre.ethers.getContract('ForumFactoryV2')
-
-	const deterministicDeployment = await deterministic('ForumCrowdfund', {
-		contract: 'ForumCrowdfund',
+	const deterministicDeployment = await deterministic('CrowdfundExecutionManager', {
+		contract: 'CrowdfundExecutionManager',
 		from: deployer,
-		args: [ForumFactory.address],
+		args: [deployer],
 		log: true,
 		autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
 		maxFeePerGas: hre.ethers.BigNumber.from('95000000000'),
@@ -22,5 +20,5 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 	await deterministicDeployment.deploy()
 }
 export default func
-func.id = 'deploy_Crowdfund' // id required to prevent reexecution
-func.tags = ['ForumCrowdfund', 'Forum']
+func.id = 'deploy_CrowdfundExecutionManager' // id required to prevent reexecution
+func.tags = ['CrowdfundExecutionManager', 'Forum']
