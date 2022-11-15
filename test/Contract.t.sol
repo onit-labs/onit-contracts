a10abc0a130c84d920de305a0817d2329c433609
jamesmcc-design <JAMES.MCC94@HOTMAIL.CO.UK>
2022-11-15 11:05:32 +0100

    feat: update git, add basic contract test

---
+++ test/Contract.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+contract ContractTest is DSTest {
+    function setUp() public {}
+
+    function test() public {
+        assertTrue(true);
+    }
+}
