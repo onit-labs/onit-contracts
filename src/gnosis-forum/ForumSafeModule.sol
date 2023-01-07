3ce3565390eff812477a7e625dd6955632a610b4
jamesmcc-design <JAMES.MCC94@HOTMAIL.CO.UK>
2023-01-07 22:18:48 +0100

    feat: add arbitrary execution fn to module

--- src/gnosis-forum/ForumSafeModule.sol
+++ src/gnosis-forum/ForumSafeModule.sol
@@ -64,11 +64,11 @@ contract ForumSafeModule is
 
 	error NotExtension();
 
-	error PFPFailed();
-
 	error SignatureError();
 
 	error CallError();
+
+	error AvatarOnly();
 
 	/// ----------------------------------------------------------------------------------------
 	///							DAO STORAGE
@@ -500,8 +500,29 @@ contract ForumSafeModule is
 	///							UTILITIES
 	/// ----------------------------------------------------------------------------------------
 
+	modifier avatarOnly() {
+		if (msg.sender != avatar) revert AvatarOnly();
+		_;
+	}
+
 	function uri(uint256 tokenId) public view override returns (string memory) {
 		return IPfpStaker(pfpExtension).getUri(address(this), name, tokenId);
+	}
+
+	/**
+	 * @notice Execute a transaction as a module
+	 * @dev Can be used to execute arbitrary code if needed, only when called by safe
+	 * @param _to address to send transaction to
+	 * @param _value value to send with transaction
+	 * @param _data data to send with transaction
+	 */
+	function executeAsModule(
+		address _to,
+		uint256 _value,
+		bytes calldata _data
+	) external avatarOnly {
+		(bool success, ) = _to.call{value: _value}(_data);
+		if (!success) revert CallError();
 	}
 
 	receive() external payable virtual {}
