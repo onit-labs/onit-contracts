2bdc33d5e7833e85c0b3c0a128093b34fd031b1c
jamesmccomish <james.mcc94@hotmail.co.uk>
2024-03-30 17:10:27 +0100

    wip: adding base 1271

--- src/onit-safe/OnitSafe.sol
+++ src/onit-safe/OnitSafe.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.23;
 import {Safe} from "../../lib/safe-smart-account/contracts/Safe.sol";
 import {WebAuthn} from "../../lib/webauthn-sol/src/WebAuthn.sol";
 import {Onit4337Wrapper, PackedUserOperation} from "../Onit4337Wrapper.sol";
+import {ERC1271} from "../utils/ERC1271.sol";
 
 import "../../lib/forge-std/src/console.sol";
 
@@ -12,7 +13,7 @@ import "../../lib/forge-std/src/console.sol";
  * @author Onit Labs (https://onit.fun)
  * @custom:warning This contract has not been audited, and is likely to change.
  */
-contract OnitSafe is Safe, Onit4337Wrapper {
+contract OnitSafe is Safe, Onit4337Wrapper, ERC1271 {
     /// ----------------------------------------------------------------------------------------
     ///							ACCOUNT STORAGE
     /// ----------------------------------------------------------------------------------------
@@ -67,11 +68,30 @@ contract OnitSafe is Safe, Onit4337Wrapper {
         // TODO use safe execution fn here?
         _call(target, value, data);
     }
+
+    // TODO
+    function domainSeparator() public view override(Safe, ERC1271) returns (bytes32) {}
+
     /// ----------------------------------------------------------------------------------------
     ///							INTERNAL METHODS
     /// ----------------------------------------------------------------------------------------
 
-    // TODO consider nonce validation in here as well in on v6 entrypoint
+    /**
+     * @notice Validate the signature of a message for ERC1271
+     * @param message   The message whose signature has been performed on
+     * @param signature The signature associated with `message`
+     * @return `true` is the signature is valid, else `false`
+     * // TODO
+     */
+    function _validateSignature(bytes32 message, bytes calldata signature) internal view override returns (bool) {
+        return WebAuthn.verify({
+            challenge: abi.encodePacked(message),
+            requireUV: false,
+            webAuthnAuth: abi.decode(signature, (WebAuthn.WebAuthnAuth)),
+            x: _owner[0],
+            y: _owner[1]
+        });
+    }
 
     /**
      * @inheritdoc Onit4337Wrapper
@@ -99,6 +119,9 @@ contract OnitSafe is Safe, Onit4337Wrapper {
     function _validateNonce(uint256 nonce) internal view virtual override {
         // TODO
     }
+
+    // TODO
+    function _domainNameAndVersion() internal view override(ERC1271) returns (string memory, string memory) {}
 
     /// @dev To ensure that only the owner or the account itself can upgrade the implementation.
     function _authorizeUpgrade(address) internal virtual override {
