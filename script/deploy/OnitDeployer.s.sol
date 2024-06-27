ed72b7440e29b4198223f30b7fda5ff373291da2
jamesmccomish <james.mcc94@hotmail.co.uk>
2024-06-27 18:18:41 +0200

    chore: remove old files and scripts

--- script/deploy/OnitDeployer.s.sol
+++ script/deploy/OnitDeployer.s.sol
@@ -1,62 +1,63 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.23;
+// // SPDX-License-Identifier: GPL-3.0-or-later
+// pragma solidity ^0.8.23;
 
-import {OnitAccount} from "../../src/onit-account/OnitAccount.sol";
-import {OnitAccountProxyFactory} from "../../src/onit-account/OnitAccountFactory.sol";
+// import {OnitAccount} from "../../src/onit-account/OnitAccount.sol";
+// import {OnitAccountProxyFactory} from "../../src/onit-account/OnitAccountFactory.sol";
 
-import {Script, console2} from "forge-std/Script.sol";
+// import {Script, console2} from "forge-std/Script.sol";
 
-/**
- * @notice Deploy the Onit Safe and the Onit Safe Proxy Factory
- */
-contract OnitDeployer is Script {
-    address public constant COMPATIBILITY_FALLBACK_HANDLER = address(0xfd0732Dc9E303f09fCEf3a7388Ad10A83459Ec99);
+// /**
+//  * @notice Deploy the Onit Safe and the Onit Safe Proxy Factory
+//  */
+// contract OnitDeployer is Script {
+//     address public constant COMPATIBILITY_FALLBACK_HANDLER = address(0xfd0732Dc9E303f09fCEf3a7388Ad10A83459Ec99);
 
-    string public constant ONIT_ACCOUNT_VERSION = "0.0.2";
-    uint256 public constant ONIT_ACCOUNT_NONCE = 1; // In case we need to redeploy thsi version to another address
+//     string public constant ONIT_ACCOUNT_VERSION = "0.0.2";
+//     uint256 public constant ONIT_ACCOUNT_NONCE = 1; // In case we need to redeploy thsi version to another address
 
-    string public constant ONIT_ACCOUNT_PROXY_FACTORY_VERSION = "0.0.2";
-    uint256 public constant ONIT_ACCOUNT_PROXY_FACTORY_NONCE = 1; // In case we need to redeploy thsi version to another address
+//     string public constant ONIT_ACCOUNT_PROXY_FACTORY_VERSION = "0.0.2";
+//     uint256 public constant ONIT_ACCOUNT_PROXY_FACTORY_NONCE = 1; // In case we need to redeploy thsi version to
+// another address
 
-    function setUp() public {}
+//     function setUp() public {}
 
-    function run() public {
-        address onitAccount;
-        address onitAccountProxyFactory;
+//     function run() public {
+//         address onitAccount;
+//         address onitAccountProxyFactory;
 
-        bytes32 ONIT_ACCOUNT_SALT = keccak256(abi.encode("onit-account", ONIT_ACCOUNT_VERSION, ONIT_ACCOUNT_NONCE));
-        bytes32 ONIT_ACCOUNT_PROXY_FACTORY_SALT = keccak256(
-            abi.encode(
-                "onit-account-proxy-factory", ONIT_ACCOUNT_PROXY_FACTORY_VERSION, ONIT_ACCOUNT_PROXY_FACTORY_NONCE
-            )
-        );
+//         bytes32 ONIT_ACCOUNT_SALT = keccak256(abi.encode("onit-account", ONIT_ACCOUNT_VERSION, ONIT_ACCOUNT_NONCE));
+//         bytes32 ONIT_ACCOUNT_PROXY_FACTORY_SALT = keccak256(
+//             abi.encode(
+//                 "onit-account-proxy-factory", ONIT_ACCOUNT_PROXY_FACTORY_VERSION, ONIT_ACCOUNT_PROXY_FACTORY_NONCE
+//             )
+//         );
 
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
+//         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+//         vm.startBroadcast(deployerPrivateKey);
 
-        bytes memory onitAccountInitCode = type(OnitAccount).creationCode;
+//         bytes memory onitAccountInitCode = type(OnitAccount).creationCode;
 
-        assembly {
-            onitAccount := create2(0, add(onitAccountInitCode, 0x20), mload(onitAccountInitCode), ONIT_ACCOUNT_SALT)
-        }
+//         assembly {
+//             onitAccount := create2(0, add(onitAccountInitCode, 0x20), mload(onitAccountInitCode), ONIT_ACCOUNT_SALT)
+//         }
 
-        bytes memory onitAccountProxyFactoryInitCode = abi.encodePacked(
-            type(OnitAccountProxyFactory).creationCode, abi.encode(COMPATIBILITY_FALLBACK_HANDLER, onitAccount)
-        );
+//         bytes memory onitAccountProxyFactoryInitCode = abi.encodePacked(
+//             type(OnitAccountProxyFactory).creationCode, abi.encode(COMPATIBILITY_FALLBACK_HANDLER, onitAccount)
+//         );
 
-        assembly {
-            onitAccountProxyFactory :=
-                create2(
-                    0,
-                    add(onitAccountProxyFactoryInitCode, 0x20),
-                    mload(onitAccountProxyFactoryInitCode),
-                    ONIT_ACCOUNT_PROXY_FACTORY_SALT
-                )
-        }
+//         assembly {
+//             onitAccountProxyFactory :=
+//                 create2(
+//                     0,
+//                     add(onitAccountProxyFactoryInitCode, 0x20),
+//                     mload(onitAccountProxyFactoryInitCode),
+//                     ONIT_ACCOUNT_PROXY_FACTORY_SALT
+//                 )
+//         }
 
-        console2.log("Onit Safe: ", onitAccount);
-        console2.log("Onit Safe Factory: ", onitAccountProxyFactory);
+//         console2.log("Onit Safe: ", onitAccount);
+//         console2.log("Onit Safe Factory: ", onitAccountProxyFactory);
 
-        vm.stopBroadcast();
-    }
-}
+//         vm.stopBroadcast();
+//     }
+// }
--- test/config/SignatureHelper.t.sol
+++ test/config/SignatureHelper.t.sol
@@ -1,46 +1,46 @@
-// SPDX-License-Identifier UNLICENSED
-pragma solidity ^0.8.13;
+// // SPDX-License-Identifier UNLICENSED
+// pragma solidity ^0.8.13;
 
-/* solhint-disable no-console */
+// /* solhint-disable no-console */
 
-import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
-import {Test} from "forge-std/Test.sol";
+// import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+// import {Test} from "forge-std/Test.sol";
 
-/**
- * @notice - This contract runs the signatureHelper.ts script
- * 			 It is used to create and sign messages, similar to how the passkey would, for testing
- */
-contract SignatureHelper is Test {
-    function createPublicKey(string memory salt) public returns (uint256[2] memory) {
-        string[] memory cmd = new string[](6);
+// /**
+//  * @notice - This contract runs the signatureHelper.ts script
+//  * 			 It is used to create and sign messages, similar to how the passkey would, for testing
+//  */
+// contract SignatureHelper is Test {
+//     function createPublicKey(string memory salt) public returns (uint256[2] memory) {
+//         string[] memory cmd = new string[](6);
 
-        cmd[0] = "yarn";
-        cmd[1] = "--silent";
-        cmd[2] = "ts-node";
-        cmd[3] = "script/signatureHelper.ts";
-        cmd[4] = "generate";
-        cmd[5] = salt;
+//         cmd[0] = "yarn";
+//         cmd[1] = "--silent";
+//         cmd[2] = "ts-node";
+//         cmd[3] = "script/signatureHelper.ts";
+//         cmd[4] = "generate";
+//         cmd[5] = salt;
 
-        bytes memory res = vm.ffi(cmd);
-        uint256[2] memory publicKey = abi.decode(res, (uint256[2]));
+//         bytes memory res = vm.ffi(cmd);
+//         uint256[2] memory publicKey = abi.decode(res, (uint256[2]));
 
-        return publicKey;
-    }
+//         return publicKey;
+//     }
 
-    function signMessageForPublicKey(string memory salt, string memory message) public returns (uint256[2] memory) {
-        string[] memory cmd = new string[](7);
+//     function signMessageForPublicKey(string memory salt, string memory message) public returns (uint256[2] memory) {
+//         string[] memory cmd = new string[](7);
 
-        cmd[0] = "yarn";
-        cmd[1] = "--silent";
-        cmd[2] = "ts-node";
-        cmd[3] = "script/signatureHelper.ts";
-        cmd[4] = "sign";
-        cmd[5] = salt;
-        cmd[6] = message;
+//         cmd[0] = "yarn";
+//         cmd[1] = "--silent";
+//         cmd[2] = "ts-node";
+//         cmd[3] = "script/signatureHelper.ts";
+//         cmd[4] = "sign";
+//         cmd[5] = salt;
+//         cmd[6] = message;
 
-        bytes memory res = vm.ffi(cmd);
-        uint256[2] memory sig = abi.decode(res, (uint256[2]));
+//         bytes memory res = vm.ffi(cmd);
+//         uint256[2] memory sig = abi.decode(res, (uint256[2]));
 
-        return sig;
-    }
-}
+//         return sig;
+//     }
+// }
