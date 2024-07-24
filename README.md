4c4d568fd0b3bc897ad170eaeb813717f81cd79c
jamesmccomish <james.mcc94@hotmail.co.uk>
2024-07-24 20:51:23 +0900

    chore: update readme and deployer script

--- README.md
+++ README.md
@@ -27,8 +27,8 @@ Onit groups will be extendedable with additional functionality, such as Fundrais
 
 | Contract              |                                                            Address                                                            | Description                                     |
 | :-------------------- | :---------------------------------------------------------------------------------------------------------------------------: | :---------------------------------------------- |
- Onit Account         | tbd (local testing) | Smart wallet with EOA or passkey owners   |
-| Onit Account Factory | tbd (local testing) | Factory for Onit Accounts                      |
+ Onit Account         | [0xe324DA4192aA61684717C2F8C994Ee0C34B15B72](https://sepolia.basescan.org/address/0xe324da4192aa61684717c2f8c994ee0c34b15b72#code) | Smart wallet with EOA or passkey owners   |
+| Onit Account Factory | [0x8472B610aDEabC68ebb27069EE18d88dF7254B2B](https://sepolia.basescan.org/address/0x8472b610adeabc68ebb27069ee18d88df7254b2b#code) | Factory for Onit Accounts                      |
 
 <br>
 <br>
--- script/deploy/OnitDeployer.s.sol
+++ script/deploy/OnitDeployer.s.sol
@@ -1,63 +1,31 @@
 // // SPDX-License-Identifier: GPL-3.0-or-later
-// pragma solidity ^0.8.23;
+pragma solidity ^0.8.13;
 
-// import {OnitAccount} from "../../src/onit-account/OnitAccount.sol";
-// import {OnitAccountProxyFactory} from "../../src/onit-account/OnitAccountFactory.sol";
+import {Script, console2} from "forge-std/Script.sol";
+import {SafeSingletonDeployer} from "safe-singleton-deployer-sol/src/SafeSingletonDeployer.sol";
 
-// import {Script, console2} from "forge-std/Script.sol";
+import {OnitSmartWallet, OnitSmartWalletFactory} from "../../lib/onit-smart-wallet/src/OnitSmartWalletFactory.sol";
 
-// /**
-//  * @notice Deploy the Onit Safe and the Onit Safe Proxy Factory
-//  */
-// contract OnitDeployer is Script {
-//     address public constant COMPATIBILITY_FALLBACK_HANDLER = address(0xfd0732Dc9E303f09fCEf3a7388Ad10A83459Ec99);
+contract OnitDeployer is Script {
+    // address constant EXPECTED_IMPLEMENTATION = 0x000100abaad02f1cfC8Bbe32bD5a564817339E72;
+    // address constant EXPECTED_FACTORY = 0x0BA5ED0c6AA8c49038F819E587E2633c4A9F428a;
 
-//     string public constant ONIT_ACCOUNT_VERSION = "0.0.2";
-//     uint256 public constant ONIT_ACCOUNT_NONCE = 1; // In case we need to redeploy thsi version to another address
+    function run() public {
+        console2.log("Deploying on chain ID", block.chainid);
 
-//     string public constant ONIT_ACCOUNT_PROXY_FACTORY_VERSION = "0.0.2";
-//     uint256 public constant ONIT_ACCOUNT_PROXY_FACTORY_NONCE = 1; // In case we need to redeploy thsi version to
-// another address
+        address implementation = SafeSingletonDeployer.broadcastDeploy({
+            creationCode: type(OnitSmartWallet).creationCode,
+            salt: keccak256("OnitSmartWallet")
+        });
+        console2.log("implementation", implementation);
+        //assert(implementation == EXPECTED_IMPLEMENTATION);
 
-//     function setUp() public {}
-
-//     function run() public {
-//         address onitAccount;
-//         address onitAccountProxyFactory;
-
-//         bytes32 ONIT_ACCOUNT_SALT = keccak256(abi.encode("onit-account", ONIT_ACCOUNT_VERSION, ONIT_ACCOUNT_NONCE));
-//         bytes32 ONIT_ACCOUNT_PROXY_FACTORY_SALT = keccak256(
-//             abi.encode(
-//                 "onit-account-proxy-factory", ONIT_ACCOUNT_PROXY_FACTORY_VERSION, ONIT_ACCOUNT_PROXY_FACTORY_NONCE
-//             )
-//         );
-
-//         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-//         vm.startBroadcast(deployerPrivateKey);
-
-//         bytes memory onitAccountInitCode = type(OnitAccount).creationCode;
-
-//         assembly {
-//             onitAccount := create2(0, add(onitAccountInitCode, 0x20), mload(onitAccountInitCode), ONIT_ACCOUNT_SALT)
-//         }
-
-//         bytes memory onitAccountProxyFactoryInitCode = abi.encodePacked(
-//             type(OnitAccountProxyFactory).creationCode, abi.encode(COMPATIBILITY_FALLBACK_HANDLER, onitAccount)
-//         );
-
-//         assembly {
-//             onitAccountProxyFactory :=
-//                 create2(
-//                     0,
-//                     add(onitAccountProxyFactoryInitCode, 0x20),
-//                     mload(onitAccountProxyFactoryInitCode),
-//                     ONIT_ACCOUNT_PROXY_FACTORY_SALT
-//                 )
-//         }
-
-//         console2.log("Onit Safe: ", onitAccount);
-//         console2.log("Onit Safe Factory: ", onitAccountProxyFactory);
-
-//         vm.stopBroadcast();
-//     }
-// }
+        address factory = SafeSingletonDeployer.broadcastDeploy({
+            creationCode: type(OnitSmartWalletFactory).creationCode,
+            args: abi.encode(implementation),
+            salt: keccak256("OnitSmartWalletFactory")
+        });
+        console2.log("factory", factory);
+        //  assert(factory == EXPECTED_FACTORY);
+    }
+}
